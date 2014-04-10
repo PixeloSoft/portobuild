7f71017acdd60814e03caef79f62348a3ab6d5ea
Usman Tahir <ch.usman.tahir@gmail.com>
2014-04-11 03:19:58 +0500

    -

--- php/container.php
+++ php/container.php
@@ -2,22 +2,22 @@
 	<?php include 'php/titleBar.php'; ?>
 	<div id="data-container">
 		<div class="gallery">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
-			<img alt="Image Thumbnail" src="images/thumb.png" class="thumb">
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
+			<a href="#"><img alt="Image Thumbnail" src="images/thumb.png" class="thumb"></a>
 		</div>
 	</div>
 </section>
