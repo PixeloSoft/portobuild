99fd4926a5c8e3fe56f1544fdff2f977eb970ba2
Usman Tahir <ch.usman.tahir@gmail.com>
2014-04-19 07:24:25 +0500

    Fancy Box navigation dots added to admin panel

--- admin/js/navigationScript.js
+++ admin/js/navigationScript.js
@@ -3,9 +3,28 @@ $(document).ready(function() {
         openEasing  : 'swing',
         closeEasing : 'swing',
         nextEasing  : 'swing',
-        prevEasing  : 'swing'
+        prevEasing  : 'swing',
+        padding     : 5,
+        afterLoad   : addLinks,
+        beforeClose : removeLinks
     });
 });
+
+function addLinks() {
+    var list = $("#links");    
+    if (!list.length) {    
+        list = $('<ul id="links">');
+        for (var i = 0; i < this.group.length; i++) {
+            $('<li data-index="' + i + '"><label></label></li>').click(function() { $.fancybox.jumpto( $(this).data('index'));}).appendTo( list );
+        }   
+        list.appendTo( 'body' );
+    }
+    list.find('li').removeClass('activeDot').eq( this.index ).addClass('activeDot');
+}
+
+function removeLinks() {
+    $("#links").remove();    
+}
 
 /* Angular JS */
 var myApp = angular.module('portobuild', []);
