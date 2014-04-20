9fcb31f4f7c1f66a1b4028bf197d2ac06656ae34
Usman Tahir <ch.usman.tahir@gmail.com>
2014-04-20 05:46:05 +0500

    New Theme Added, To run, open localhost/portobuild/themes/SideMenu/ on local machine

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="pb-index">
+	<?php include 'php/head.php'; ?>
+	<body>
+
+	</body>
+	<div class="clearfix"></div>
+
+	<!-- All JS Files -->
+	<script src="_lib/jquery-1.11.0.min.js"></script>
+	<script src="_lib/bootstrap/js/bootstrap.min.js"></script>
+	<script src="js/indexScript.js"></script>
+</html>
